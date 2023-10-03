fbec9953b5385705242b79e2b2841cc6c10e3017
Veselin Doychev <veselindoi123@gmail.com>
2023-10-03 18:01:44 +0300

    Caught exception if tries to take non-existent key

--- intermediate/nato_alphabet_csv/main.py
+++ intermediate/nato_alphabet_csv/main.py
@@ -1,6 +1,5 @@
 import pandas
 
-
 nato_data = pandas.read_csv('nato_phonetic_alphabet.csv')
 # print(nato_data.to_dict())
 
@@ -8,9 +7,15 @@ result = {row.letter: row.code for index, row in nato_data.iterrows()}
 print(result)
 
 
-while True:
+def generate_phonetic():
     word_input = input(f"Enter a word: ")
-    filtered_word = [result[ch.upper()] for ch in word_input]
+    try:
+        filtered_word = [result[ch.upper()] for ch in word_input]
+    except KeyError:
+        print(f"Sorry only letters in the alphabet please.")
+        generate_phonetic()
+    else:
+        print(filtered_word)
 
-    print(filtered_word)
 
+generate_phonetic()
