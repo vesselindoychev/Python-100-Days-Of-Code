b08641e54b4bb090bad1aee4e5bc0358efd5d822
Veselin Doychev <veselindoi123@gmail.com>
2023-10-10 15:24:30 +0300

    Checked if a weather code is less than 700, which means weather will be in bad condition to print a message

--- intermediate/weather_app/main.py
+++ intermediate/weather_app/main.py
@@ -15,8 +15,26 @@ url = 'https://api.openweathermap.org/data/3.0/onecall'
 weather_params = {
     'lat': lat,
     'lon': long,
-    'appid': api_key
+    'appid': api_key,
+    'exclude': 'current,minutely,daily'
 }
 
 response = requests.get(url=url, params=weather_params)
-print(response.json())
+response.raise_for_status()
+weather_data = response.json()
+
+weather_data_by_hours = weather_data['hourly']
+print(weather_data_by_hours)
+
+is_bad_condition = False
+for hourly_data in range(0, 12):
+    weather = weather_data_by_hours[hourly_data]['weather']
+    weather_id = weather[0]['id']
+    if int(weather_id) < 700:
+        is_bad_condition = True
+    print(weather_id)
+
+if is_bad_condition:
+    print('Bring an umbrella')
+print(weather_data)
+
