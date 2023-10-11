f9032da639ea9211f984f9db98afafb7cda5540c
Veselin Doychev <veselindoi123@gmail.com>
2023-10-11 13:08:38 +0300

    Calculated the percent of the two last days of closing prices and took the last 3 news from an api enpoint, then sent sms text to phone number via Twilio

--- intermediate/stock_trading_new_alert_app/main.py
+++ intermediate/stock_trading_new_alert_app/main.py
@@ -1,11 +1,17 @@
 import requests
 import os
+from twilio.rest import Client
 from dotenv import load_dotenv
-import datetime
 
 load_dotenv(verbose=True)
 
-STOCK = "TSLA"
+account_sid = os.environ['TWILIO_ACCOUNT_SID']
+auth_token = os.environ['TWILIO_AUTH_TOKEN']
+sender_phone_number = os.environ['TWILIO_PHONE_NUMBER']
+receiver_phone_number = os.environ['RECEIVER_PHONE_NUMBER']
+client = Client(account_sid, auth_token)
+
+STOCK_NAME = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
 STOCK_API_KEY = os.environ['STOCK_API_KEY']
@@ -16,45 +22,58 @@ news_url = 'https://newsapi.org/v2/everything'
 
 stock_parameters = {
     'function': 'TIME_SERIES_DAILY',
-    'symbol': 'IBM',
-    # 'interval': '5min',
+    'symbol': STOCK_NAME,
     'apikey': STOCK_API_KEY,
 }
 
 news_parameters = {
-    'q': 'tesla',
-    'from': '2023-09-10',
-    'sortBy': 'publishedAt',
+    'q': COMPANY_NAME,
     'apiKey': NEWS_API_KEY
 }
 
 stock_response = requests.get(url=stock_url, params=stock_parameters)
 stock_data = stock_response.json()['Time Series (Daily)']
-print(stock_data)
 
 news_response = requests.get(url=news_url, params=news_parameters)
 news_data = news_response.json()
-print(news_data)
 
+news_articles = news_data['articles'][:3]
 
+text = ''
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+counter = 0
 
-## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+closed_prices = []
 
-## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+for k, v in stock_data.items():
+    if counter == 2:
+        break
 
+    closed_prices.append(v['4. close'])
+    counter += 1
 
-# Optional: Format the SMS message like this:
-"""
-TSLA: 🔺2%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-or
-"TSLA: 🔻5%
-Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
-Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
-"""
+yesterday_closing_price = float(closed_prices[0])
+day_before_yesterday_closing_price = float(closed_prices[1])
+
+difference = yesterday_closing_price - day_before_yesterday_closing_price
+
+diff_percent = (difference / yesterday_closing_price) * 100
+print(difference)
+print(diff_percent)
+up_down = None
+
+if difference > 0:
+    up_down = '▲'
+else:
+    up_down = '▼'
+
+for article in news_articles:
+    text = f"{STOCK_NAME}: {up_down}{round(diff_percent)}%\n" \
+           f"Headline: {article['title']}. ({STOCK_NAME})?\n" \
+           f"Brief: {article['description']}\n"
+
+    message = client.messages.create(
+        body=text,
+        from_=sender_phone_number,
+        to=receiver_phone_number
+    )
