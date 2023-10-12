a121b7c1ec4778a141e85d20cc47b0f65e8d8409
Veselin Doychev <veselindoi123@gmail.com>
2023-10-12 11:48:24 +0300

    Connected Sheety with Google Sheets and through api post request automatically fill the sheets

--- intermediate/workout_tracking_using_google_sheets/main.py
+++ intermediate/workout_tracking_using_google_sheets/main.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from dotenv import load_dotenv
+from datetime import datetime
 
 load_dotenv(verbose=True)
 
@@ -15,7 +16,6 @@ MY_AGE = 20
 exercise_endpoint = 'https://trackapi.nutritionix.com/v2/natural/exercise'
 
 exercise_input = input('Tell me which exercises you did: ')
-
 
 post_request_body = {
     "query": exercise_input,
@@ -33,3 +33,27 @@ headers = {
 response = requests.post(url=exercise_endpoint, json=post_request_body, headers=headers)
 result = response.json()
 print(result)
+
+current_date = datetime.now().strftime('%d/%m/%Y')
+current_time = datetime.now().strftime('%H:%M:%S')
+
+SHEETY_USERNAME = os.environ['SHEETY_USERNAME']
+SHEETY_PROJECT_NAME = os.environ['SHEETY_PROJECT_NAME']
+SHEETY_NAME = os.environ['SHEETY_NANME']
+
+sheety_endpoint = f'https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT_NAME}/{SHEETY_NAME}'
+
+# sheety_response = requests.get(url=sheety_endpoint)
+# print(sheety_response.json())
+for exercise in result['exercises']:
+    row_content = {
+        'workout': {
+            'date': current_date,
+            'time': current_time,
+            'exercise': exercise['name'].title(),
+            'duration': exercise['duration_min'],
+            'calories': exercise['nf_calories']
+        }
+    }
+    sheety_post_response = requests.post(url=sheety_endpoint, json=row_content)
+    print(sheety_post_response.json())
