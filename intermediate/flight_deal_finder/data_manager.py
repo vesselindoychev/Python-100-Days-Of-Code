a40668b3f6bce476b28f3a20470781d0eeb45f49
Veselin Doychev <veselindoi123@gmail.com>
2023-10-12 14:32:49 +0300

    Added get request to sheety

--- intermediate/flight_deal_finder/data_manager.py
+++ intermediate/flight_deal_finder/data_manager.py
@@ -1,4 +1,5 @@
 import os
+import requests
 from dotenv import load_dotenv
 load_dotenv(verbose=True)
 
@@ -7,9 +8,13 @@ class DataManager:
         self.sheety_project_name = os.environ['SHEETY_PROJECT_NAME_FLIGHT']
         self.sheety_username = os.environ['SHEETY_USERNAME']
         self.sheety_name = os.environ['SHEETY_NAME']
-        self.sheety_auth_token = os.environ['SHEETY_TOKEN']
+        self.sheety_auth_token = os.environ['SHEETY_AUTH_TOKEN']
 
         self.sheety_headers = {
             'Authorization': f'Bearer {self.sheety_auth_token}'
         }
         self.sheety_api_endpoint = f'https://api.sheety.co/{self.sheety_username}/{self.sheety_project_name}/{self.sheety_name}'
+
+    def get_data_with_sheety_api(self):
+        response = requests.get(url=self.sheety_api_endpoint, headers=self.sheety_headers)
+        print(response.json())
