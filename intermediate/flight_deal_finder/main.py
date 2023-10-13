38e2bdcb0fdcecc3521a05a206b7bb4725042b82
Veselin Doychev <veselindoi123@gmail.com>
2023-10-13 15:35:26 +0300

    Changed cityCode and updated the destination data

--- intermediate/flight_deal_finder/main.py
+++ intermediate/flight_deal_finder/main.py
@@ -1,1 +1,16 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+# This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from flight_search import FlightSearch
+import requests
+
+data_manager = DataManager()
+flight_search = FlightSearch()
+
+sheet_data = data_manager.get_destination_data()
+
+if sheet_data[0]['iataCode'] == '':
+    for item in sheet_data:
+        item['iataCode'] = flight_search.get_destination_code(item['city'])
+
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
