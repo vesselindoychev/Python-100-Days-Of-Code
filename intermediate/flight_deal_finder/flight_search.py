a06c5f6fb59b76118860b75855a5d76dad0e83f4
Veselin Doychev <veselindoi123@gmail.com>
2023-10-13 15:36:30 +0300

    Added funtion, which only return hardcoded cityCode

--- intermediate/flight_deal_finder/flight_search.py
+++ intermediate/flight_deal_finder/flight_search.py
@@ -1,3 +1,5 @@
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    def get_destination_code(self, city_name):
+        code = 'TESTING'
+        return code
+
