af777992427db75573c5635e329ae18ba745426b
Veselin Doychev <veselindoi123@gmail.com>
2023-10-13 17:39:45 +0300

    Take a current iataCode from an api enpoint and return it

--- intermediate/flight_deal_finder/flight_search.py
+++ intermediate/flight_deal_finder/flight_search.py
@@ -1,5 +1,23 @@
+import os
+
+import requests
+from dotenv import load_dotenv
+
+load_dotenv(verbose=True)
+
+TEQUILA_KIWI_API_ENDPOINT = 'https://api.tequila.kiwi.com'
+
+KIWI_API_KEY = os.environ['KIWI_API_KEY']
+KIWI_HEADERS = {
+    'apikey': KIWI_API_KEY,
+}
+
+
 class FlightSearch:
     def get_destination_code(self, city_name):
-        code = 'TESTING'
+        location_api_endpoint = f'{TEQUILA_KIWI_API_ENDPOINT}/locations/query'
+        query = {'term': city_name, 'location_types': 'city'}
+        response = requests.get(url=location_api_endpoint, headers=KIWI_HEADERS, params=query)
+        result = response.json()['locations']
+        code = result[0]['code']
         return code
-
