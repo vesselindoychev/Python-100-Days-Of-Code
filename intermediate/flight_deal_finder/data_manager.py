0638aeebbfd77526b1307260b3994059e10b98bb
Veselin Doychev <veselindoi123@gmail.com>
2023-10-13 15:34:00 +0300

    Changed  data manager file

--- intermediate/flight_deal_finder/data_manager.py
+++ intermediate/flight_deal_finder/data_manager.py
@@ -1,20 +1,40 @@
+from flight_search import FlightSearch
 import os
 import requests
+from pprint import pprint
 from dotenv import load_dotenv
+
 load_dotenv(verbose=True)
+
+SHEETY_PROJECT_NAME = os.environ['SHEETY_PROJECT_NAME_FLIGHT']
+SHEETY_USERNAME = os.environ['SHEETY_USERNAME']
+SHEETY_NAME = os.environ['SHEETY_NAME']
+SHEETY_AUTH_TOKEN = os.environ['SHEETY_AUTH_TOKEN']
+
+SHEETY_HEADERS = {
+    'Authorization': f'Bearer {SHEETY_AUTH_TOKEN}'
+}
+SHEETY_API_ENDPOINT = f'https://api.sheety.co/{SHEETY_USERNAME}/{SHEETY_PROJECT_NAME}/{SHEETY_NAME}'
+
 
 class DataManager:
     def __init__(self):
-        self.sheety_project_name = os.environ['SHEETY_PROJECT_NAME_FLIGHT']
-        self.sheety_username = os.environ['SHEETY_USERNAME']
-        self.sheety_name = os.environ['SHEETY_NAME']
-        self.sheety_auth_token = os.environ['SHEETY_AUTH_TOKEN']
+        self.destination_data = {}
 
-        self.sheety_headers = {
-            'Authorization': f'Bearer {self.sheety_auth_token}'
-        }
-        self.sheety_api_endpoint = f'https://api.sheety.co/{self.sheety_username}/{self.sheety_project_name}/{self.sheety_name}'
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_API_ENDPOINT, headers=SHEETY_HEADERS)
+        sheet_data = response.json()
+        self.destination_data = sheet_data['prices']
+        return self.destination_data
 
-    def get_data_with_sheety_api(self):
-        response = requests.get(url=self.sheety_api_endpoint, headers=self.sheety_headers)
-        print(response.json())
+    def update_destination_codes(self):
+        for item in self.destination_data:
+            request_body = {
+                'price': {
+                    'iataCode': item['iataCode']
+                }
+            }
+
+            response = requests.put(url=f"{SHEETY_API_ENDPOINT}/{item['id']}", json=request_body,
+                                    headers=SHEETY_HEADERS)
+            print(response.text)
