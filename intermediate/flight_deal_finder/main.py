e6525d01bcfba90569ed755909adf61570fee795
Veselin Doychev <veselindoi123@gmail.com>
2023-10-16 17:42:48 +0300

    Added functionality to call a fuction, which sends email

--- intermediate/flight_deal_finder/main.py
+++ intermediate/flight_deal_finder/main.py
@@ -3,7 +3,10 @@ from flight_search import FlightSearch
 import requests
 from datetime import datetime
 from datetime import timedelta
+from customer_data import CustomerService
+from notification_manager import NotificationManager
 
+notification_manager = NotificationManager()
 data_manager = DataManager()
 flight_search = FlightSearch()
 
@@ -16,14 +19,27 @@ if sheet_data[0]['iataCode'] == '':
     data_manager.destination_data = sheet_data
     data_manager.update_destination_codes()
 
-ORIGIN_CITY_CODE = 'LGW'
+ORIGIN_CITY_CODE = 'LON'
 tomorrow = datetime.now().date() + timedelta(days=1)
 six_months_from_today = datetime.now().date() + timedelta(days=(6 * 30))
 
 for destination in sheet_data:
+    text = ''
+    iataCode = destination['iataCode']
     flight = flight_search.check_flights(
         ORIGIN_CITY_CODE,
-        destination['iataCode'],
+        iataCode,
         from_time=tomorrow,
         to_time=six_months_from_today,
     )
+
+    if flight is None:
+        continue
+
+    if int(flight.price) < int(destination['lowestPrice']):
+        text += f'Low price alert! Only £{flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}.'
+
+        if flight.stop_overs > 0:
+            text += f"\nFlight has {flight.stop_overs} stop over, via {flight.via_city}."
+
+        notification_manager.send_email(text)
