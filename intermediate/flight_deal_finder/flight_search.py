5a175cf265bf8c1423e369d93a3a6dde825b1e7c
Veselin Doychev <veselindoi123@gmail.com>
2023-10-16 14:03:37 +0300

    Added a funtion, which searches with api a given destination with params

--- intermediate/flight_deal_finder/flight_search.py
+++ intermediate/flight_deal_finder/flight_search.py
@@ -1,5 +1,5 @@
 import os
-
+from flight_data import FlightData
 import requests
 from dotenv import load_dotenv
 
@@ -21,3 +21,38 @@ class FlightSearch:
         result = response.json()['locations']
         code = result[0]['code']
         return code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
+        query = {
+            'fly_from': origin_city_code,
+            'fly_to': destination_city_code,
+            'date_from': from_time.strftime('%d/%m/%Y'),
+            'date_to': to_time.strftime('%d/%m/%Y'),
+            'nights_in_dst_from': 7,
+            'nights_in_dst_to': 28,
+            'one_for_city': 1,
+            'max_stopovers': 0,
+            'curr': 'GBP'
+        }
+
+        response = requests.get(url=f"{TEQUILA_KIWI_API_ENDPOINT}/v2/search", headers=KIWI_HEADERS, params=query)
+
+        try:
+            data = response.json()['data'][0]
+        except IndexError:
+            print(f"No flights found for {destination_city_code}")
+            return None
+
+        flight_data = FlightData(
+            price=data['price'],
+            origin_city=data['route'][0]['cityFrom'],
+            origin_airport=data['route'][0]['flyFrom'],
+            destination_city=data['route'][0]['cityTo'],
+            destination_airport=data['route'][0]['flyTo'],
+            out_date=data['route'][0]['local_departure'].split('T')[0],
+            return_date=data['route'][1]['local_departure'].split('T')[0]
+        )
+
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        flight_data.check_for_lowest_price()
+        return flight_data
