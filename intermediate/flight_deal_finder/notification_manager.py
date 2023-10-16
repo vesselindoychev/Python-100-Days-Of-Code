f3d8585e3bb29145492296466f1f9ab03c621652
Veselin Doychev <veselindoi123@gmail.com>
2023-10-16 14:02:00 +0300

    A whole class, which send a twilio sms

--- intermediate/flight_deal_finder/notification_manager.py
+++ intermediate/flight_deal_finder/notification_manager.py
@@ -1,3 +1,19 @@
+import os
+from twilio.rest import Client
+
+TWILIO_ACC_SID = os.environ['TWILIO_ACC_SID']
+TWILIO_AUTH_TOKEN = os.environ['TWILIO_AUTH_TOKEN']
+SENDER_PHONE_NUMBER = os.environ['TWILIO_PHONE_NUMBER']
+RECEIVER_PHONE_NUMBER = os.environ['TWILIO_RECEIVER_PHONE_NUMBER']
+
+
 class NotificationManager:
-    #This class is responsible for sending notifications with the deal flight details.
-    pass
+    def __init__(self):
+        self.client = Client(TWILIO_ACC_SID, TWILIO_AUTH_TOKEN)
+
+    def send_sms(self, text):
+        message = self.client.messages.create(
+            from_=SENDER_PHONE_NUMBER,
+            body=text,
+            to=RECEIVER_PHONE_NUMBER
+        )
