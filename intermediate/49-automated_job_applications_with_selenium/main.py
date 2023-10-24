c90279448361c161b405c652b797dfbb53712191
Veselin Doychev <veselindoi123@gmail.com>
2023-10-24 17:11:06 +0300

    Added for loop in order the bot to be able to go through all jobs and saves them

--- intermediate/49-automated_job_applications_with_selenium/main.py
+++ intermediate/49-automated_job_applications_with_selenium/main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import os
 from dotenv import load_dotenv
+
 load_dotenv(verbose=True)
 
 email = os.environ['LINKEDIN_EMAIL']
@@ -28,7 +29,6 @@ nav_btns = driver.find_elements(By.CSS_SELECTOR, value='header nav div a')
 sign_in_btn = nav_btns[1]
 sign_in_btn.click()
 
-
 # time.sleep(5)
 
 email_input = driver.find_element(By.ID, value='username')
@@ -37,10 +37,13 @@ email_input.send_keys(email)
 password_input = driver.find_element(By.ID, value='password')
 password_input.send_keys(password)
 
-
 confirm_sign_in_btn = driver.find_element(By.CSS_SELECTOR, value='.login__form_action_container button')
 confirm_sign_in_btn.send_keys(Keys.ENTER)
 
-save_job_btn = driver.find_element(By.CLASS_NAME, value='jobs-save-button')
-save_job_btn.send_keys(Keys.ENTER)
-
+time.sleep(5)
+all_job_ads = driver.find_elements(By.CSS_SELECTOR, value='.job-card-container--clickable')
+for job in all_job_ads:
+    job.click()
+    time.sleep(2)
+    save_job_btn = driver.find_element(By.CLASS_NAME, value='jobs-save-button')
+    save_job_btn.send_keys(Keys.ENTER)
