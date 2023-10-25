e2602a73096aa0150357cd9f50014526d96d81bc
Veselin Doychev <veselindoi123@gmail.com>
2023-10-25 14:25:48 +0300

    Accepted facebook cookie then automate fill the login inputs and hit login button

--- intermediate/50-auto_tinder_swiping_bot_with_selenium/main.py
+++ intermediate/50-auto_tinder_swiping_bot_with_selenium/main.py
@@ -2,13 +2,16 @@ import time
 from selenium import webdriver
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
+import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
 
-# service = Service(ChromeDriverManager().install())
+
 tinder_url = 'https://tinder.com'
 
-# chrome_driver_path = '/path/to/chromedriver'
+EMAIL = os.environ['FACEBOOK_EMAIL']
+PASSWORD = os.environ['FACEBOOK_PASSWORD']
+
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_experimental_option('detach', True)
 
@@ -22,6 +25,27 @@ a_tag.click()
 time.sleep(2)
 login_link = driver.find_element(By.XPATH, '//*[@id="s-561743307"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
 login_link.send_keys(Keys.ENTER)
+
+time.sleep(2)
+base_window = driver.window_handles[0]
+fb_login_window = driver.window_handles[1]
+driver.switch_to.window(fb_login_window)
+print(driver.title)
+
+allow_cookies_btn = driver.find_element(By.XPATH, '/html/body/div[2]/div[2]/div/div/div/div/div[4]/button[2]')
+allow_cookies_btn.send_keys(Keys.ENTER)
+
+time.sleep(2)
+email_input = driver.find_element(By.ID, value='email')
+email_input.send_keys(EMAIL)
+
+password_input = driver.find_element(By.ID, value='pass')
+password_input.send_keys(PASSWORD)
+
+facebook_login_btn = driver.find_element(By.NAME, value='login')
+facebook_login_btn.click()
+
+
 #
 # facebook_login_link = driver.find_element(By.XPATH, value='//*[@id="s-561743307"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
 # print(facebook_login_link.get_attribute('aria-label'))
