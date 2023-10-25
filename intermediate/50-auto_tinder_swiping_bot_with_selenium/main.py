23c683b7ef2cc02c4ba820deacbadc94b40c5e66
Veselin Doychev <veselindoi123@gmail.com>
2023-10-25 15:09:07 +0300

    Changed while loop to for, because we have only 100 likes per day

--- intermediate/50-auto_tinder_swiping_bot_with_selenium/main.py
+++ intermediate/50-auto_tinder_swiping_bot_with_selenium/main.py
@@ -1,11 +1,12 @@
 import time
 from selenium import webdriver
+from selenium.common import NoSuchElementException
 from selenium.webdriver import Keys
 from selenium.webdriver.common.by import By
 import os
 from dotenv import load_dotenv
-load_dotenv(verbose=True)
 
+load_dotenv(verbose=True)
 
 tinder_url = 'https://tinder.com'
 
@@ -19,11 +20,13 @@ driver = webdriver.Chrome(options=chrome_options)
 driver.get(url=tinder_url)
 
 time.sleep(2)
-a_tag = driver.find_element(By.XPATH, '//*[@id="s1166637769"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
+a_tag = driver.find_element(By.XPATH,
+                            '//*[@id="s1166637769"]/div/div[1]/div/main/div[1]/div/div/div/div/header/div/div[2]/div[2]/a')
 a_tag.click()
 
 time.sleep(2)
-login_link = driver.find_element(By.XPATH, '//*[@id="s-561743307"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
+login_link = driver.find_element(By.XPATH,
+                                 '//*[@id="s-561743307"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
 login_link.send_keys(Keys.ENTER)
 
 time.sleep(2)
@@ -45,8 +48,33 @@ password_input.send_keys(PASSWORD)
 facebook_login_btn = driver.find_element(By.NAME, value='login')
 facebook_login_btn.click()
 
+time.sleep(2)
+driver.switch_to.window(base_window)
+print(driver.title)
 
-#
-# facebook_login_link = driver.find_element(By.XPATH, value='//*[@id="s-561743307"]/main/div[1]/div/div[1]/div/div/div[2]/div[2]/span/div[2]/button')
-# print(facebook_login_link.get_attribute('aria-label'))
-# facebook_login_link.click()
+tinder_cookie_btn = driver.find_element(By.XPATH, '//*[@id="s-561743307"]/main/div[2]/div/div/div[1]/div[1]/button')
+tinder_cookie_btn.click()
+
+time.sleep(2)
+tinder_share_location_btn = driver.find_element(By.XPATH, '//*[@id="s-561743307"]/main/div/div/div/div[3]/button[1]')
+tinder_share_location_btn.click()
+
+time.sleep(2)
+disable_notifications_btn = driver.find_element(By.XPATH, '//*[@id="s-561743307"]/main/div/div/div/div[3]/button[2]')
+disable_notifications_btn.click()
+
+for i in range(100):
+    time.sleep(2)
+    try:
+        like_btn = driver.find_element(By.XPATH,
+                                       '//*[@id="s1166637769"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div/div[4]/button')
+        like_btn.click()
+    except Exception:
+        try:
+            match_popup = driver.find_element(By.CSS_SELECTOR, value='.itsAMatch a')
+            match_popup.click()
+        except NoSuchElementException:
+            time.sleep(2)
+
+driver.quit()
+
