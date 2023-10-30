d50394dfeb19297e736de445536ab9d4de208460
Veselin Doychev <veselindoi123@gmail.com>
2023-10-30 16:47:35 +0200

    Added funtionality to make google forms and fill automatically it

--- intermediate/53-web_scraping_capstone_project/main.py
+++ intermediate/53-web_scraping_capstone_project/main.py
@@ -1,14 +1,24 @@
+import time
+
 from bs4 import BeautifulSoup
 import requests
 import os
 from dotenv import load_dotenv
 from selenium import webdriver
-
+from selenium.common import ElementClickInterceptedException
+from selenium.webdriver.common.by import By
 
 load_dotenv(verbose=True)
 
+CHROME_PATH = os.environ['CHROME_PATH']
+CHROME_OPTIONS = webdriver.ChromeOptions()
+CHROME_OPTIONS.add_experimental_option('detach', True)
+
 USER_AGENT = os.environ['USER_AGENT']
 ACCEPT_LANG = os.environ['ACCEPT_LANGUAGE']
+
+EMAIL = os.environ['GOOGLE_EMAIL']
+PASSWORD = os.environ['GOOGLE_PASSWORD']
 
 HEADERS = {
     'User-Agent': USER_AGENT,
@@ -25,7 +35,9 @@ PROPERTY_CLASS = 'ListItem-c11n-8-84-3__sc-10e22w8-0'
 
 
 class FindProperty:
-    def __init__(self):
+    def __init__(self, chrome_path):
+        self.service = webdriver.ChromeService(executable_path=chrome_path)
+        self.driver = webdriver.Chrome(options=CHROME_OPTIONS, service=self.service)
         self.soup = BeautifulSoup(WEBPAGE_TEXT, 'lxml')
         self.prices = []
         self.links = []
@@ -44,8 +56,12 @@ class FindProperty:
     def get_links_of_property_ads(self):
         main_url = 'https://www.zillow.com'
         all_property_a_tags = self.soup.find_all(name='a', class_='property-card-link')
-        for prop_a in all_property_a_tags:
-            link = main_url + prop_a.get('href')
+        for prop_a in range(0, len(all_property_a_tags) - 1, 2):
+            prop_link = all_property_a_tags[prop_a].get('href').split(':')[0]
+            if prop_link == 'https':
+                link = all_property_a_tags[prop_a].get('href')
+            else:
+                link = main_url + all_property_a_tags[prop_a].get('href')
             self.links.append(link)
 
     def get_all_property_prices(self):
@@ -55,7 +71,7 @@ class FindProperty:
                 price = p.text.split('+')
             else:
                 price = p.text.split('/')
-                self.prices.append(price[0])
+            self.prices.append(price[0])
 
     def get_all_property_addresses(self):
         all_addresses = self.soup.select('[data-test=property-card-addr]')
@@ -66,8 +82,37 @@ class FindProperty:
                 address = addr.text
             self.addresses.append(address)
 
-find_property = FindProperty()
+    def make_google_form(self):
+        self.driver.get(GOOGLE_FORM_LINK)
+
+        time.sleep(2)
+
+        for i in range(len(self.prices)):
+            if i != 0:
+                send_another_answer_btn = self.driver.find_element(By.XPATH, '/html/body/div[1]/div[2]/div[1]/div/div[4]/a')
+                send_another_answer_btn.click()
+                time.sleep(3)
+            address_input = self.driver.find_element(By.XPATH,
+                                                     '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            price_input = self.driver.find_element(By.XPATH,
+                                                   '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div[1]/input')
+            link_input = self.driver.find_element(By.XPATH,
+                                                  '//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div[1]/input')
+
+            address_input.send_keys(self.addresses[i])
+            price_input.send_keys(self.prices[i])
+            link_input.send_keys(self.links[i])
+
+            send_form_btn = self.driver.find_element(By.XPATH, '//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+            send_form_btn.click()
+            time.sleep(4)
+
+        self.driver.quit()
+
+
+find_property = FindProperty(CHROME_PATH)
 find_property.get_all_properties()
 find_property.get_links_of_property_ads()
 find_property.get_all_property_prices()
 find_property.get_all_property_addresses()
+find_property.make_google_form()
