1b21ba77c5bab111df79b6c4022e0b816c619af8
Veselin Doychev <veselindoi123@gmail.com>
2023-10-31 16:25:34 +0200

    Added advanced decorator

---
+++ intermediate/54-introduction_to_flask/demo.py
@@ -0,0 +1,22 @@
+class User:
+    def __init__(self, name):
+        self.name = name
+        self.is_logged_in = False
+
+
+def is_authenticated_decorator(func):
+    def wrapper(*args):
+        if args[0].is_logged_in:
+            func(args[0])
+
+    return wrapper
+
+
+@is_authenticated_decorator
+def create_blog_post(user):
+    print(f"This is {user.name}' s new blog post")
+
+
+new_user = User('vesko')
+new_user.is_logged_in = True
+create_blog_post(new_user)
