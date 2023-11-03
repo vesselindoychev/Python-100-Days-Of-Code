f217f834b65770346cfb70956971911ccf398950
Veselin Doychev <veselindoi123@gmail.com>
2023-11-03 11:40:16 +0200

    Rendered templates

--- intermediate/57-blog-capstone-project/main.py
+++ intermediate/57-blog-capstone-project/main.py
@@ -1,11 +1,18 @@
+import requests
 from flask import Flask, render_template
-
 
 app = Flask(__name__)
 
+
 @app.route('/')
 def home():
-    return render_template("index.html")
+    blog_data_response = requests.get(url='https://api.npoint.io/ae124e0a00e39c2791f2')
+    blog_data = blog_data_response.json()
+    context = {
+        'blog_data': blog_data,
+    }
+    return render_template("index.html", **context)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
