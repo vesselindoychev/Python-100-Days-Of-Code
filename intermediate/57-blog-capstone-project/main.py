70407fbf33f39a10aaf5590e0d5a64fe6385df7b
Veselin Doychev <veselindoi123@gmail.com>
2023-11-06 14:37:33 +0200

    Added two urls for blog and contact page

--- intermediate/57-blog-capstone-project/main.py
+++ intermediate/57-blog-capstone-project/main.py
@@ -2,14 +2,8 @@ import requests
 from post import Post
 from flask import Flask, render_template
 
-blog_data_response = requests.get(url='https://api.npoint.io/ae124e0a00e39c2791f2')
+blog_data_response = requests.get(url='https://api.npoint.io/eb6cd8a5d783f501ee7d')
 blog_data = blog_data_response.json()
-
-blog_objects = []
-
-for blog in blog_data:
-    blog_obj = Post(blog['id'], blog['title'], blog['subtitle'], blog['body'])
-    blog_objects.append(blog_obj)
 
 app = Flask(__name__)
 
@@ -17,7 +11,7 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     context = {
-        'blogs': blog_objects,
+        'blogs': blog_data,
     }
     return render_template("index.html", **context)
 
@@ -25,14 +19,23 @@ def home():
 @app.route('/post/<int:id>')
 def show_blog_data(id):
     current_blog = None
-    for blog in blog_objects:
-        if blog.id == id:
+    for blog in blog_data:
+        if blog['id'] == id:
             current_blog = blog
     context = {
         'blog': current_blog
     }
     return render_template('post.html', **context)
 
+
+@app.route('/about')
+def show_about_page():
+    return render_template('about.html')
+
+
+@app.route('/contact')
+def show_contact_page():
+    return render_template('contact.html')
 
 if __name__ == "__main__":
     app.run(debug=True)
