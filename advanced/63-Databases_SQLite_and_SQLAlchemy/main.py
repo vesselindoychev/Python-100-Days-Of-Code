9c6788d56f714e8e8962dfa15b8078f1a7e6442b
Veselin Doychev <veselindoi123@gmail.com>
2023-11-08 15:35:48 +0200

    Created Database with SQLAlchemy and added POST operation

--- advanced/63-Databases_SQLite_and_SQLAlchemy/main.py
+++ advanced/63-Databases_SQLite_and_SQLAlchemy/main.py
@@ -2,11 +2,28 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap4
 from flask_wtf import FlaskForm
 from wtforms import StringField, validators, FloatField, SubmitField
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'some secret'
-all_books = []
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///books-collection.sqlite3'
+
+# all_books = []
 bootstrap4 = Bootstrap4(app)
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 class BookForm(FlaskForm):
@@ -18,6 +35,8 @@ class BookForm(FlaskForm):
 
 @app.route('/')
 def home():
+    with app.app_context():
+        all_books = db.session.query(Book).all()
     return render_template('index.html', books=all_books)
 
 
@@ -25,12 +44,16 @@ def home():
 def add():
     book_form = BookForm()
     if book_form.validate_on_submit():
-        all_books.append({
-            'title': book_form.title.data,
-            'author': book_form.author.data,
-            'rating': book_form.rating.data
-        })
-        print(all_books)
+        # all_books.append({
+        #     'title': book_form.title.data,
+        #     'author': book_form.author.data,
+        #     'rating': book_form.rating.data
+        # })
+        # print(all_books)
+        with app.app_context():
+            new_book = Book(title=book_form.title.data, author=book_form.author.data, rating=book_form.rating.data)
+            db.session.add(new_book)
+            db.session.commit()
         return redirect(url_for('home'))
     return render_template('add.html', book_form=book_form)
 
