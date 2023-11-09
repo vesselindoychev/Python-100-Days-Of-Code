5c066e0f98ff981ac09f1ceadcfc9581d68b8b64
Veselin Doychev <veselindoi123@gmail.com>
2023-11-09 12:35:21 +0200

    Added db with 2 examples

--- advanced/64-Top_10_Movies_Website/main.py
+++ advanced/64-Top_10_Movies_Website/main.py
@@ -6,22 +6,29 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests
 
-'''
-Red underlines? Install the required packages first: 
-Open the Terminal in PyCharm (bottom left). 
-
-On Windows type:
-python -m pip install -r requirements.txt
-
-On MacOS type:
-pip3 install -r requirements.txt
-
-This will install the packages from requirements.txt for this project.
-'''
-
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
-Bootstrap5(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///movies-collection.sqlite3'
+
+bootstrap5 = Bootstrap5(app)
+
+db = SQLAlchemy()
+db.init_app(app)
+
+
+class Movie(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50), unique=True, nullable=False)
+    year = db.Column(db.Integer, nullable=False)
+    description = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+    ranking = db.Column(db.Integer, nullable=False)
+    review = db.Column(db.String(25), nullable=False)
+    img_url = db.Column(db.String(250), nullable=False)
+
+
+with app.app_context():
+    db.create_all()
 
 
 @app.route("/")
