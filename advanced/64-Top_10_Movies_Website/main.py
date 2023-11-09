1279abc4a077b2f1dcc96ae59838ea384cb1dd84
Veselin Doychev <veselindoi123@gmail.com>
2023-11-09 13:36:27 +0200

    Created Edit form and function with route

--- advanced/64-Top_10_Movies_Website/main.py
+++ advanced/64-Top_10_Movies_Website/main.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import desc
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
+from wtforms import StringField, SubmitField, FloatField, validators
 from wtforms.validators import DataRequired
 import requests
 
@@ -31,9 +32,32 @@ with app.app_context():
     db.create_all()
 
 
+class EditMovieRatingForm(FlaskForm):
+    rating = FloatField(label='Your rating Out of 10 e.g 7.5', validators=[validators.DataRequired()])
+    review = StringField(label='Your Review', validators=[validators.DataRequired()])
+    submit = SubmitField(label='Done')
+
+
 @app.route("/")
 def home():
-    return render_template("index.html")
+    with app.app_context():
+        all_movies = db.session.query(Movie).order_by(Movie.ranking.desc()).all()
+    return render_template("index.html", movies=all_movies)
+
+
+@app.route('/edit/<int:id>', methods=['GET', 'POST'])
+def edit(id):
+    form = EditMovieRatingForm()
+    current_movie = db.session.execute(db.select(Movie).where(Movie.id == id)).scalar()
+
+    if form.validate_on_submit():
+        if request.method == 'POST':
+            current_movie.rating = form.rating.data
+            current_movie.review = form.review.data
+            db.session.commit()
+            return redirect(url_for('home'))
+
+    return render_template('edit.html', form=form, movie=current_movie)
 
 
 if __name__ == '__main__':
