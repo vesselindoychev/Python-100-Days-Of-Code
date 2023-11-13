05b99d81de67eaecaf59daa9bef26985c6db896e
Veselin Doychev <veselindoi123@gmail.com>
2023-11-13 17:01:04 +0200

    Added delete view with route and contact view for sending email to the admin of the site

--- advanced/57-blog-capstone-project/main.py
+++ advanced/57-blog-capstone-project/main.py
@@ -1,13 +1,15 @@
 import datetime
+import smtplib
 
 from flask import Flask, render_template, redirect, url_for, request
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, fields, validators
-from wtforms.validators import DataRequired, URL
+from wtforms import fields, validators
 from flask_ckeditor import CKEditor, CKEditorField
-from datetime import date
+import os
+from dotenv import load_dotenv
+load_dotenv(verbose=True)
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
@@ -20,6 +22,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
 db = SQLAlchemy()
 db.init_app(app)
 
+RECEIVER_EMAIL = os.environ['RECEIVER_EMAIL']
+PASSWORD = os.environ['PASSWORD']
 
 # CONFIGURE TABLE
 class BlogPost(db.Model):
@@ -79,8 +83,6 @@ def create_new_post():
     return render_template('make-post.html', form=form)
 
 
-
-
 @app.route('/edit-post/<int:post_id>', methods=['GET', 'POST'])
 def edit_post(post_id):
     today = datetime.datetime.now()
@@ -108,7 +110,13 @@ def edit_post(post_id):
     return render_template('make-post.html', form=form, is_edit=True)
 
 
-# TODO: delete_post() to remove a blog post from the database
+@app.route('/delete/<int:post_id>')
+def delete_post(post_id):
+    post = db.session.execute(db.select(BlogPost).where(BlogPost.id == post_id)).scalar()
+    db.session.delete(post)
+    db.session.commit()
+    return redirect(url_for('get_all_posts'))
+
 
 # Below is the code from previous lessons. No changes needed.
 @app.route("/about")
@@ -116,9 +124,42 @@ def about():
     return render_template("about.html")
 
 
-@app.route("/contact")
+@app.route("/contact", methods=['GET', 'POST'])
 def contact():
-    return render_template("contact.html")
+    if request.method == 'POST':
+        full_name = request.form.get('name')
+        email = request.form.get('email')
+        phone = request.form.get('phone-number')
+        message = request.form.get('message')
+
+        print(f'Full Name: {full_name}, \n'
+              f'Email: {email},\n'
+              f'Phone: {phone},\n'
+              f'Message: {message}')
+
+        context = {
+            'msg_sent': True,
+            'msg': 'Successfully sent your message.'
+        }
+
+        send_email(email, message, full_name)
+        return render_template('contact.html', **context)
+    context = {
+        'msg_sent': False
+    }
+
+    return render_template('contact.html', **context)
+
+
+def send_email(sender_mail, text, name):
+    with smtplib.SMTP('smtp.gmail.com') as connection:
+        connection.starttls()
+        connection.login(user=RECEIVER_EMAIL, password=PASSWORD)
+        connection.sendmail(
+            from_addr=RECEIVER_EMAIL,
+            to_addrs=RECEIVER_EMAIL,
+            msg=f'Subject:Sent message by email: {sender_mail}\n\nName:{name}\n{text}'
+        )
 
 
 if __name__ == "__main__":
