8c498a52da3b09146c885d0cec5f9858526d9fa3
Veselin Doychev <veselindoi123@gmail.com>
2023-11-14 17:44:06 +0200

    Added login manager, user_loader, login form and modified the secrets view

--- advanced/68-Authentication_with_Flask/main.py
+++ advanced/68-Authentication_with_Flask/main.py
@@ -18,13 +18,25 @@ db = SQLAlchemy()
 db.init_app(app)
 Bootstrap5(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def user_loader(user_id):
+    return db.get_or_404(User, user_id)
+
 
 # CREATE TABLE IN DB
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
+    authenticated = db.Column(db.Boolean, default=False)
+
+    def is_authenticated(self):
+        return self.authenticated
 
 
 with app.app_context():
@@ -42,6 +54,14 @@ class RegistrationForm(FlaskForm):
                                             render_kw={'placeholder': 'Confirm Password'})
     submit = fields.SubmitField('Sign up')
 
+
+class LoginForm(FlaskForm):
+    email = fields.EmailField(validators=[validators.DataRequired(), validators.Email()],
+                              render_kw={'placeholder': 'Email'})
+    password = fields.PasswordField(validators=[validators.DataRequired()], render_kw={'placeholder': 'Password'})
+    submit = fields.SubmitField('Log in')
+
+
 @app.route('/')
 def home():
     return render_template("index.html")
@@ -55,34 +75,56 @@ def register():
         email = form.email.data
         password = form.password.data
 
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user:
+            return render_template('register.html', form=form, is_existing=True)
+
+        hashed_password = generate_password_hash(password, salt_length=16)
+
         new_user = User(
             name=name,
             email=email,
-            password=password
+            password=hashed_password
         )
 
         db.session.add(new_user)
         db.session.commit()
 
-        data = json.dumps({'user_name': new_user.name})
-        session['data'] = data
-        return redirect(url_for('secrets', data=data))
+        login_user(new_user)
+        return redirect(url_for('secrets'))
+        # data = json.dumps({'name': new_user.name})
+        # session['data'] = data
+        # return redirect(url_for('secrets', data=data))
+
     return render_template("register.html", form=form)
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    form = LoginForm()
+
+    if form.validate_on_submit():
+        email = form.email.data
+        password = form.password.data
+        user = db.session.execute(db.select(User).where(User.email == email)).scalar()
+        if user:
+            if check_password_hash(user.password, password):
+                user.authenticated = True
+                db.session.commit()
+                login_user(user)
+                return redirect(url_for('secrets'))
+    return render_template("login.html", form=form)
 
 
 @app.route('/secrets')
+@login_required
 def secrets():
-    data = request.args['data']
-    data = session['data']
-    user_name = json.loads(data)
-    name = user_name['user_name']
-    print(name)
-    return render_template("secrets.html", name=name)
+    # data = request.args['data']
+    # data = session['data']
+    # name = json.loads(data)['name']
+    # print(name)
+    print(current_user.name)
+    return render_template("secrets.html", name=current_user.name)
 
 
 @app.route('/logout')
@@ -91,6 +133,7 @@ def logout():
 
 
 @app.route('/download', methods=['GET', 'POST'])
+@login_required
 def download():
     path = 'files/cheat_sheet.pdf'
     return send_from_directory('static', as_attachment=True, path=path)
