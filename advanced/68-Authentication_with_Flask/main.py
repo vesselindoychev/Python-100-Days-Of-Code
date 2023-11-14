c6226e3618f3ef82841b059fdd3817b672ed3833
Veselin Doychev <veselindoi123@gmail.com>
2023-11-14 13:09:03 +0200

    Created registration form and register view

--- advanced/68-Authentication_with_Flask/main.py
+++ advanced/68-Authentication_with_Flask/main.py
@@ -1,7 +1,12 @@
-from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory
+import json
+
+from flask import Flask, render_template, request, url_for, redirect, flash, send_from_directory, session
+from flask_bootstrap import Bootstrap5
+from flask_wtf import FlaskForm
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin, login_user, LoginManager, login_required, current_user, logout_user
+from wtforms import fields, validators
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret-key-goes-here'
@@ -10,6 +15,7 @@ app.config['SECRET_KEY'] = 'secret-key-goes-here'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 db = SQLAlchemy()
 db.init_app(app)
+Bootstrap5(app)
 
 
 # CREATE TABLE IN DB
@@ -18,20 +24,49 @@ class User(db.Model):
     email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(1000))
- 
- 
+
+
 with app.app_context():
     db.create_all()
 
+
+class RegistrationForm(FlaskForm):
+    name = fields.StringField(validators=[validators.DataRequired(), validators.Length(min=3, max=20)],
+                              render_kw={'placeholder': 'Name'})
+    email = fields.EmailField(validators=[validators.DataRequired(), validators.Email()],
+                              render_kw={'placeholder': 'Email'})
+    password = fields.PasswordField(validators=[validators.DataRequired(), validators.Length(min=5)],
+                                    render_kw={'placeholder': 'Password'})
+    confirm_password = fields.PasswordField(validators=[validators.DataRequired(), validators.EqualTo('password')],
+                                            render_kw={'placeholder': 'Confirm Password'})
+    submit = fields.SubmitField('Sign up')
 
 @app.route('/')
 def home():
     return render_template("index.html")
 
 
-@app.route('/register')
+@app.route('/register', methods=['GET', 'POST'])
 def register():
-    return render_template("register.html")
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        email = form.email.data
+        password = form.password.data
+
+        new_user = User(
+            name=name,
+            email=email,
+            password=password
+        )
+
+        db.session.add(new_user)
+        db.session.commit()
+
+        data = json.dumps({'user_name': new_user.name})
+        session['data'] = data
+        return redirect(url_for('secrets', data=data))
+    return render_template("register.html", form=form)
 
 
 @app.route('/login')
@@ -41,7 +76,12 @@ def login():
 
 @app.route('/secrets')
 def secrets():
-    return render_template("secrets.html")
+    data = request.args['data']
+    data = session['data']
+    user_name = json.loads(data)
+    name = user_name['user_name']
+    print(name)
+    return render_template("secrets.html", name=name)
 
 
 @app.route('/logout')
