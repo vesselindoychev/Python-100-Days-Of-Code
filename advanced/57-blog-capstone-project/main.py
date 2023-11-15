ec3bfbc2c4f13f3b143b78643aaf7c7522bdeb6b
Veselin Doychev <veselindoi123@gmail.com>
2023-11-15 17:48:39 +0200

    Created register form

--- advanced/57-blog-capstone-project/main.py
+++ advanced/57-blog-capstone-project/main.py
@@ -51,6 +51,22 @@ with app.app_context():
     db.create_all()
 
 
+class RegistrationForm(FlaskForm):
+    first_name = fields.StringField(label='First Name',
+                                    validators=[validators.DataRequired(), validators.Length(min=3)],
+                                    render_kw={'placeholder': 'Ivan'})
+    last_name = fields.StringField(label='Last Name', validators=[validators.DataRequired(), validators.Length(min=3)],
+                                   render_kw={'placeholder': 'Petrov'})
+    email = fields.EmailField(label='Email', validators=[validators.DataRequired(), validators.Email()],
+                              render_kw={'placeholder': 'ivan123@gmail.com'})
+    password = fields.PasswordField(label='Password', validators=[validators.DataRequired(), validators.Length(min=5)],
+                                    render_kw={'placeholder': f'{5 * "*"}'})
+    confirm_password = fields.PasswordField(label='Confirm Password',
+                                            validators=[validators.DataRequired(), validators.EqualTo('password')],
+                                            render_kw={'placeholder': f'Retype your password'})
+    submit = fields.SubmitField(label='Sign up')
+
+
 class CreateNewPost(FlaskForm):
     title = fields.StringField(label='Blog Post Title', validators=[validators.DataRequired()])
     subtitle = fields.StringField(label='Subtitle', validators=[validators.DataRequired()])
